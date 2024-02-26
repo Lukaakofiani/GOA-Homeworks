ca8854b0ec46b57207a6586b31d7ab2def6f5959
luka <127971095+Lukaakofiani@users.noreply.github.com>
2024-02-26 05:11:56 -0800

    Delete Day 10/Homework directory

--- Day 10/Homework/test.py
+++
@@ -1,7 +0,0 @@
-answer = ""
-while answer != "Yes":
-    answer = input("Are you 18 years old?: ")
-    if answer == "Yes":
-        print("You are Eligible!")
-    else:
-        print("Try again")
